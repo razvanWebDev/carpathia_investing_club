eca09bdc9651f24ec61e71c72d56d13b4ac94fad
razvanWebDev <crsn_razvan@yahoo.com>
2021-07-27 17:48:35 +0300

    added member requests to admin dashboard

--- admin/admin.php
+++ admin/admin.php
@@ -35,19 +35,24 @@
 
             <!-- ./col -->
 
-            <!-- <div class="col-lg-3 col-6">
-              <div class="small-box bg-success">
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-warning">
                 <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <?php
+                  $query = "SELECT * FROM member_requests";
+                  $result = mysqli_query($connection, $query);
+                  $num_requests = mysqli_num_rows($result);
+                  ?>
+                  <h3><?php echo $num_requests ?></h3>
 
-                  <p>Bounce Rate</p>
+                  <p>Memeber Requests</p>
                 </div>
                 <div class="icon">
-                  <i class="ion ion-stats-bars"></i>
+                  <i class="fas fa-user-clock"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="member-requests.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
-            </div> -->
+            </div>
 
             <!-- ./col -->
 
